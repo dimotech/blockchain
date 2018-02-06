f8f5d71a37ae3cf72a5b6b9aa1e46f176bb8715b
Di Mo <dimotech18@gmail.com>
2018-02-06 11:17:16 -0800

    Decentralizing Blockchain
    1) Creating Node Model
    2) Passing Arguments to the Node App from command line
    3) Register Nodes
    4) Mining on Nodes
    5) Resolving Conflicts

--- app.js
+++ app.js
@@ -1,20 +1,71 @@
 let Block = require("./block")
 let Blockchain = require("./blockchain")
+let BlockchainNode = require("./BlockchainNode")
 let Transaction = require("./transaction")
+
+let fetch = require("node-fetch")
 
 const express = require("express")
 const app = express()
 const bodyParser = require("body-parser")
 
-let transactions = []
+let port = 3000
 
+//access the arguments
+process.argv.forEach(function(val, index, array){
+	port = array[2]
+})
+
+if (port == undefined) {
+	port = 3000
+}
+
+let transactions = []
+let nodes = []
 let genesisBlock = new Block()
 let blockchain = new Blockchain(genesisBlock)
 
 app.use(bodyParser.json())
 
+app.get("/resolve", function(req, res){
+
+	nodes.forEach(function(node) {
+
+		fetch(node.url + '/blockchain')
+		.then(function(response) {
+			return response.json()
+		})
+		.then(function(otherNodeBlockchain) {
+			
+			if (blockchain.blocks.length < otherNodeBlockchain.blocks.length) {
+				blockchain = otherNodeBlockchain
+			}
+
+			res.send(blockchain)
+		})
+
+	})
+
+})
+
+app.post("/nodes/register", function(req, res){
+
+	let nodesLists = req.body.urls
+	nodesLists.forEach(function(nodeDictionary) {
+		let node = new BlockchainNode(nodeDictionary["url"])
+		nodes.push(node)
+	})
+
+	res.json(nodes)
+
+})
+
 app.get("/", function(req, res) {
 	res.send("hello world")
+})
+
+app.get("/nodes", function(req, res) {
+	res.json(nodes)
 })
 
 app.post("/transactions", function(req, res) {
@@ -35,6 +86,7 @@ app.get("/mine", function(req, res) {
 
 	let block = blockchain.getNextBlock(transactions)
 	blockchain.addBlock(block)
+	transactions = []
 	res.json(block);
 
 }) 
@@ -60,6 +112,6 @@ app.get("/blockchain", function(req, res){
 
 })
 
-app.listen(3000, function(){
+app.listen(port, function(){
 	console.log("server has started")
 })
