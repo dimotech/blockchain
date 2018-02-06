66e6d38da8da4c3a2d4501828dd8f1c17edf7d14
Di Mo <dimotech18@gmail.com>
2018-02-06 09:22:08 -0800

    implementing blockchain web api
    1) setting up express
    2) adding transactions
    3) mining blocks

--- app.js
+++ app.js
@@ -1,18 +1,65 @@
 let Block = require("./block")
 let Blockchain = require("./blockchain")
-
 let Transaction = require("./transaction")
-let transaction = new Transaction("Mary", "Jerry", 100)
+
+const express = require("express")
+const app = express()
+const bodyParser = require("body-parser")
+
+let transactions = []
 
 let genesisBlock = new Block()
 let blockchain = new Blockchain(genesisBlock)
 
-let block = blockchain.getNextBlock([transaction])
-blockchain.addBlock(block)
+app.use(bodyParser.json())
 
-let anotherTransaction = new Transaction("Tom", "Stephanie", 300)
-let block1 = blockchain.getNextBlock([anotherTransaction, transaction])
-blockchain.addBlock(block1)
+app.get("/", function(req, res) {
+	res.send("hello world")
+})
+
+app.post("/transactions", function(req, res) {
+
+	let to = req.body.to
+	let from = req.body.from
+	let amount = req.body.amount
+
+	let transaction = new Transaction(from, to, amount)
+
+	transactions.push(transaction)
+
+	res.json(transactions)
+
+})
+
+app.get("/mine", function(req, res) {
+
+	let block = blockchain.getNextBlock(transactions)
+	blockchain.addBlock(block)
+	res.json(block);
+
+}) 
+
+app.get("/blockchain", function(req, res){
+
+	res.json(blockchain)
+
+	// let transaction = new Transaction("Mary", "Jerry", 100)
+
+	// let genesisBlock = new Block()
+	// let blockchain = new Blockchain(genesisBlock)
+
+	// let block = blockchain.getNextBlock([transaction])
+	// blockchain.addBlock(block)
+
+	// let anotherTransaction = new Transaction("Tom", "Stephanie", 300)
+	// let block1 = blockchain.getNextBlock([anotherTransaction, transaction])
+	// blockchain.addBlock(block1)
 
 
-console.log(blockchain)
+	// res.json(blockchain)
+
+})
+
+app.listen(3000, function(){
+	console.log("server has started")
+})
